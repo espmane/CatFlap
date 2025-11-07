480864868a0a5b98e759dad2eb55f99ebb66f28a
Esp <duckerlicious@duck.com>
2025-11-07 14:36:49 +0100

    .

---
+++ src/CatFlap/CatFlap.java
@@ -0,0 +1,4 @@
+package CatFlap;
+
+public class CatFlap {
+}
---
+++ src/CatFlap/CatFlapException.java
@@ -0,0 +1,5 @@
+package CatFlap;
+
+public class CatFlapException extends RuntimeException{
+    public CatFlapException(String message){super(message);}
+}
